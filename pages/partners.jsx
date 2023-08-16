d06e83ab0d177a881d9c8053cdab4092b06ead48
issamhoh <132918078+issamhoh@users.noreply.github.com>
2023-08-16 01:38:14 +0100

    Add files via upload

---
+++ pages/partners.jsx
@@ -0,0 +1,40 @@
+
+export default function Partners() {
+
+    return (
+        <>
+            <div className="w-full my-10">
+            <div className="flex w-full justify-center">
+      <img width="400" src="/img/bck4.png" />
+      </div>
+                <h1 className="py-7 animateHeader text-4xl font-extrabold text-center text-white">
+                    <i className="fal fa-stars text-amber-400 mr-2" />
+                    Partners
+                </h1>
+            </div>
+
+            <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
+
+            <div className="flex flex-col justify-center text-white rounded">
+            <a href="https://discord.gg/eCVabJbaFN">
+                        <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
+                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/attachments/1043105813260144660/1043229095883972738/exHost.jpeg" />
+                            <div>
+                                <h1 className="leading-none text-3xl font-bold text-white">Ex- Host Development</h1>        
+                                <div className="flex items-center mt-1">
+                                    <p className="font-normal font-sm">
+                                    A Discord Server where you can code your Discord Bot for free, with Full Support & Help! Join Now, It's Free! 
+                                    </p>
+                                </div>     
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+
+            
+
+                </div>
+                <div className="py-10"></div>
+        </>
+    );
+};
---
+++ pages/privacy.jsx
@@ -0,0 +1,23 @@
+export default function Privacy() {
+
+    return (
+        <div>
+            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Yuzu</h1>
+            <p className="text-sm text-white/30 mb-4">Last Update: 11/17/2022  🆕️</p>
+  <div className="animateHeader text-white text-opacity-40 mt-5">
+        <p>⊳ To use the Yuzu, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <div className="py-3"></div>
+        <p>⊳ You have to vote for  <a className="text-white" href="https://top.gg/bot/1024893673080635483/vote">Yuzu</a> when using certain commands from the bot commands.</p>
+        <div className="py-3"></div>
+        <p>⊳ Every user can use Yuzu however they want, as long as they do not intend to harm.</p>
+        <div className="py-3"></div>
+        <p>⊳ You cannot use certain commands at the same time while using Yuzu.</p>   
+        <div className="py-3"></div>
+        <p>⊳ Yuzu does not save any data based on users.</p>
+        <div className="py-3"></div>
+        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
+    
+        </div>
+            </div>
+    );
+};
